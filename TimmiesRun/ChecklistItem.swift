d13bdac42627de4dba3839c3c563395a0a8db7e6
Jester-1987 <nicole.groeger@triosstudent.com>
2024-06-06 11:44:57 -0400

    iOSApp2

---
+++ TimmiesRun/ChecklistItem.swift
@@ -0,0 +1,12 @@
+//
+//  ChecklistItem.swift
+//  TimmiesRun
+//
+//  Created by Nicole Groeger on 2024-06-06.
+//
+
+import Foundation
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
--- TimmiesRun/TimmiesRun/TimmiesViewController.swift
+++ TimmiesRun/TimmiesRun/TimmiesViewController.swift
@@ -8,12 +8,107 @@
 import UIKit
 
 class TimmiesViewController: UITableViewController {
-
+    var items = [ChecklistItem]()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+
+      // Replace previous code with the following
+      let item1 = ChecklistItem()
+      item1.text = "Medium Double Double"
+      items.append(item1)
+
+      let item2 = ChecklistItem()
+      item2.text = "Large English Breakfast Tea"
+      items.append(item2)
+
+      let item3 = ChecklistItem()
+      item3.text = "Small Iced Capp"
+      items.append(item3)
+
+      let item4 = ChecklistItem()
+      item4.text = "Box of 20 TimBits"
+      items.append(item4)
+
+      let item5 = ChecklistItem()
+      item5.text = "Boston Cream Donut"
+      items.append(item5)
     }
 
+    var row0item = ChecklistItem()
+    var row1item = ChecklistItem()
+    var row2item = ChecklistItem()
+    var row3item = ChecklistItem()
+    var row4item = ChecklistItem()
+    
+    // MARK: - Table View Data Source
+    override func tableView(
+      _ tableView: UITableView,
+      numberOfRowsInSection section: Int
+    ) -> Int {
+        return items.count
+    }
+
+    override func tableView(
+      _ tableView: UITableView,
+      cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(
+        withIdentifier: "ChecklistItem",
+        for: indexPath)
+
+      let item = items[indexPath.row]
+
+      configureText(for: cell, with: item)
+      configureCheckmark(for: cell, with: item)
+      return cell
+    }
+
+
+    // MARK: - Table View Delegate
+    override func tableView(
+      _ tableView: UITableView,
+      didSelectRowAt indexPath: IndexPath
+    ) {
+      if let cell = tableView.cellForRow(at: indexPath) {
+        let item = items[indexPath.row]
+        item.checked.toggle()
+        configureCheckmark(for: cell, with: item)
+      }
+      tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+
+    // configure checkmarks
+    
+    func configureCheckmark(
+      for cell: UITableViewCell,
+      with item: ChecklistItem
+    ) {
+      if item.checked {
+        cell.accessoryType = .checkmark
+      } else {
+        cell.accessoryType = .none
+      }
+    }
+    
+    func configureText(
+      for cell: UITableViewCell,
+      with item: ChecklistItem
+    ) {
+      let label = cell.viewWithTag(1000) as! UILabel
+      label.text = item.text
+    }
+
+
+
+    // default checkmark states
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
 
 }
 
