3bf08af2f6e19a4f99fc6a7e28946345c6261708
Jester-1987 <nicole.groeger@triosstudent.com>
2024-06-13 10:08:37 -0400

    configured Add Item screen

--- TimmiesRun/TimmiesRun/AddItemViewController.swift
+++ TimmiesRun/TimmiesRun/AddItemViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    
+    // Text field outlet
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    // bring up keyboard automatically
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     // MARK: - Actions
@@ -23,6 +35,47 @@ class AddItemViewController: UITableViewController {
     
     // done button
     @IBAction func done() {
+        // test print to terminal
+        print("Contents of the text field: \(textField.text!)")
+        
         navigationController?.popViewController(animated: true)
     }
+    
+    // MARK: - Table View Delegates
+    
+    // stops text field from being grayed out when user taps on screen elsewhere
+    override func tableView(
+        _ tableView: UITableView,
+        willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+        return nil
+    }
+    
+    // MARK: Text Field Delegates
+    
+    // disabling done button until something is entered into the text field
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+    // clear button correctly disables the Done button after clearing text field
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
+   
+    
+
 }
